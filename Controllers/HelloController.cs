cb5ed1ccb58baa1827f31d1dacf9e846dd7e34c7
Ivaylo Kostov <ikostov87@gmail.com>
2019-07-05 00:47:53 +0300

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_library.Controllers
+{
+    public class HelloController : Controller
+    {
+        //GET: /Hello/Index
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //GET: /Hello/Welcome
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
