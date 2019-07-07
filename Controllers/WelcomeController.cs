ac1efb3779f466f87a8b69d73845ffbf5eeba787
Ivaylo Kostov <ikostov87@gmail.com>
2019-07-08 02:29:05 +0300

    Rename "Hello" constroler to "Welcome". Make changes in "_Layout" file. Added "Welcome" view folder and index file in it. Make test changes in "_ViewStart" file.

--- Controllers/WelcomeController.cs
+++ Controllers/WelcomeController.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Book_library.Controllers
 {
-    public class HelloController : Controller
+    public class WelcomeController : Controller
     {
         //GET: /Hello/Index
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         //GET: /Hello/Welcome
-        public string Welcome()
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
